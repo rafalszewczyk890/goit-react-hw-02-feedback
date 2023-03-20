d7ff748b715e07aed685db1ffb6042a799e57a02
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-03-20 13:19:29 +0100

    Step 1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = event => {
+    const { name } = event.target;
+
+    this.setState({
+      [name]: this.state[name] + 1,
+    });
+    console.log(event);
+  };
+
+  render() {
+    return (
+      <>
+        <h5>Please leave feedback</h5>
+        <button name="good" onClick={this.handleClick}>
+          Good
+        </button>
+        <button name="neutral" onClick={this.handleClick}>
+          Neutral
+        </button>
+        <button name="bad" onClick={this.handleClick}>
+          Bad
+        </button>
+
+        <h5>Statistics</h5>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </>
+    );
+  }
+}
+
+export default App;
